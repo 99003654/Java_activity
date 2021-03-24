b752c1d1302e30913825012fcc8ba2afb7dda95b
99003654 <78997681+99003654@users.noreply.github.com>
2021-03-24 12:22:56 +0530

    Update TwoWheeler.java

--- source code/Vehicle/TwoWheeler.java
+++ source code/Vehicle/TwoWheeler.java
@@ -1,1 +1,22 @@
+class TwoWheeler extends Vehicle
+{
+	private boolean kickStartAvailable;
+	
+	public boolean isKickStartAvailable() {
+		return kickStartAvailable;
+	}
+
+
+
+	public void setKickStartAvailable(boolean kickStartAvailable) {
+		this.kickStartAvailable = kickStartAvailable;
+	}
+
+
+
+	public TwoWheeler(String make, String vehicleNumber, String fuelType, int fuelCapacity, int cc,boolean kickStartAvailable) {
+		super(make, vehicleNumber, fuelType, fuelCapacity, cc);
+		//TwoWheeler Constructor 
+		this.kickStartAvailable=kickStartAvailable;
+	}
 
