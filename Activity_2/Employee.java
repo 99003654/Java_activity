00a954389234a43b36f20ca5bbc96c90e830b9e3
99003654 <78997681+99003654@users.noreply.github.com>
2021-03-25 17:05:08 +0530

    Update Employee.java

--- Activity_2/Employee.java
+++ Activity_2/Employee.java
@@ -1,1 +1,39 @@
+package random_activity;
 
+	import java.util.ArrayList;
+	import java.util.Collections;
+	import java.util.Comparator;
+
+			class Employee {
+			    private int empid;
+			    String empname;
+			    int salary;
+			  
+			    public Employee(int empid, String empname, int salary)
+			    {
+			        this.empid = empid;
+			        this.empname = empname;
+			        this.salary = salary;
+			    }
+			} 
+			class Sortbyname implements Comparator<Employee> {
+			    public int compare(Employee a, Employee b)
+			    {
+			        return a.empname.compareTo(b.empname);
+			    }
+			}
+			  
+			class Main {
+			    public static void main(String[] args)
+			    {
+			        ArrayList<Employee> ar = new ArrayList<Employee>();
+			        ar.add(new Employee(123, "manzar", 2000));
+			        ar.add(new Employee(131, "mithun", 3000));
+			        ar.add(new Employee(142, "op", 4000));
+			        
+			        Collections.sort(ar, new Sortbyname());
+			        for (int i = 0; i < ar.size(); i++)
+			            System.out.println(ar.get(i));
+			    }
+			}
+			
